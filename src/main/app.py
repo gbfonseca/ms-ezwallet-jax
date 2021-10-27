33aabea05aee3d1b430f495fab39c01f13e7e249
Gabriel Fonseca <gabriel.fonsecadev@gmail.com>
2021-10-27 00:18:36 -0300

    feat: compose GetActionsController on main Layer

---
+++ src/main/app.py
@@ -0,0 +1,14 @@
+from src.infra.scraper.selenium import ScraperAdapter
+from src.presentation.controllers.actions.get_actions import GetActions
+
+scraper_adapter = ScraperAdapter()
+get_actions = GetActions(scraper_adapter)
+
+http_request = {
+    'body': {
+        'url': 'https://fundamentus.com.br/resultado.php'
+    }
+}
+
+data = get_actions.handle(http_request)
+print(data)
