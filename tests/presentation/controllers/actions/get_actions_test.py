6adf3a71158be11e95351d7f26b0ce2b11b7dbb6
Gabriel Fonseca <gabriel.fonsecadev@gmail.com>
2021-10-31 20:23:08 -0300

    test: ensure Scraper return an actions on success

--- tests/presentation/controllers/actions/get_actions_test.py
+++ tests/presentation/controllers/actions/get_actions_test.py
@@ -3,33 +3,6 @@ from ms_ezwallet_jax.src.domain.usecases.scraper import Scraper
 from ms_ezwallet_jax.src.domain.usecases.add_actions import AddActions
 from pytest_mock import MockFixture
 
-mockReturn = [
-    {
-        "_id": 'any_id',
-        "Papel": "BBAS3",
-        "Cotação": 2917,
-        "P/L": 526,
-        "P/VP": "062",
-        "PSR": "0000",
-        "Div.Yield": "6,88%",
-        "P/Ativo": 0,
-        "P/Cap.Giro": "000",
-        "P/EBIT": "000",
-        "P/Ativ Circ.Liq": 0,
-        "EV/EBIT": "000",
-        "EV/EBITDA": "000",
-        "Mrg Ebit": "0,00%",
-        "Mrg. Líq.": "0,00%",
-        "Liq. Corr.": 0,
-        "ROIC": "0,00%",
-        "ROE": "11,73%",
-        "Liq.2meses": "419.635.000,00",
-        "Patrim. Líq": "135.444.000.000,00",
-        "Dív.Brut/ Patrim.": "000",
-        "Cresc. Rec.5a": "0,79%"
-    },
-]
-
 
 class ScraperStub(Scraper):
 
@@ -121,7 +94,32 @@ def test_success_scrap(mocker: MockFixture):
     assert response['status_code'] == 200
     assert response == {
         'status_code': 200,
-        'body': mockReturn
+        'body': [
+            {
+                "_id": 'any_id',
+                "Papel": "BBAS3",
+                "Cotação": 2917,
+                "P/L": 526,
+                "P/VP": "062",
+                "PSR": "0000",
+                "Div.Yield": "6,88%",
+                "P/Ativo": 0,
+                "P/Cap.Giro": "000",
+                "P/EBIT": "000",
+                "P/Ativ Circ.Liq": 0,
+                "EV/EBIT": "000",
+                "EV/EBITDA": "000",
+                "Mrg Ebit": "0,00%",
+                "Mrg. Líq.": "0,00%",
+                "Liq. Corr.": 0,
+                "ROIC": "0,00%",
+                "ROE": "11,73%",
+                "Liq.2meses": "419.635.000,00",
+                "Patrim. Líq": "135.444.000.000,00",
+                "Dív.Brut/ Patrim.": "000",
+                "Cresc. Rec.5a": "0,79%"
+            },
+        ]
     }
 
 
@@ -181,3 +179,41 @@ def test_get_actions_return_500_if_add_actions_throws(mocker: MockFixture):
     http_response = sut.handle(http_request)
 
     assert http_response['status_code'] == 500
+
+
+def test_scraper_return_an_actions_on_success(mocker: MockFixture):
+    [sut, scraper_stub, _] = make_sut()
+    spy = mocker.spy(scraper_stub, 'get_data')
+    http_request = {
+        'body': {
+            'url': 'https://fundamentus.com.br/resultado.php'
+        }
+    }
+
+    sut.handle(http_request)
+
+    assert spy.spy_return == [
+        {
+            "Papel": "BBAS3",
+            "Cotação": 2917,
+            "P/L": 526,
+            "P/VP": "062",
+            "PSR": "0000",
+            "Div.Yield": "6,88%",
+            "P/Ativo": 0,
+            "P/Cap.Giro": "000",
+            "P/EBIT": "000",
+            "P/Ativ Circ.Liq": 0,
+            "EV/EBIT": "000",
+            "EV/EBITDA": "000",
+            "Mrg Ebit": "0,00%",
+            "Mrg. Líq.": "0,00%",
+            "Liq. Corr.": 0,
+            "ROIC": "0,00%",
+            "ROE": "11,73%",
+            "Liq.2meses": "419.635.000,00",
+            "Patrim. Líq": "135.444.000.000,00",
+            "Dív.Brut/ Patrim.": "000",
+            "Cresc. Rec.5a": "0,79%"
+        },
+    ]
