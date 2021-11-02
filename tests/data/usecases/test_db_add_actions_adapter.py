32e07b38eb9ce6292720631c77bf023c8226efd7
Gabriel Fonseca <gabriel.fonsecadev@gmail.com>
2021-11-02 19:46:14 -0300

    test: ensure DbAddActionsAdapter calls add with correct value

--- tests/data/usecases/test_db_add_actions_adapter.py
+++ tests/data/usecases/test_db_add_actions_adapter.py
@@ -100,6 +100,42 @@ def test_db_add_actions_adapter_returns_an_actions_on_success(mocker: MockFixtur
     ]
 
 
+def test_db_add_actions_adapter_returns_an_actions_on_success(mocker: MockFixture):
+    [sut, _] = make_sut()
+
+    spy = mocker.spy(sut, 'add')
+
+    data = [
+        {
+            "Papel": "BBAS3",
+            "Cotação": 2917,
+            "P/L": 526,
+            "P/VP": "062",
+            "PSR": "0000",
+            "Div.Yield": "6,88%",
+            "P/Ativo": 0,
+            "P/Cap.Giro": "000",
+            "P/EBIT": "000",
+            "P/Ativ Circ.Liq": 0,
+            "EV/EBIT": "000",
+            "EV/EBITDA": "000",
+            "Mrg Ebit": "0,00%",
+            "Mrg. Líq.": "0,00%",
+            "Liq. Corr.": 0,
+            "ROIC": "0,00%",
+            "ROE": "11,73%",
+            "Liq.2meses": "419.635.000,00",
+            "Patrim. Líq": "135.444.000.000,00",
+            "Dív.Brut/ Patrim.": "000",
+            "Cresc. Rec.5a": "0,79%"
+        }
+    ]
+
+    response = sut.add(data)
+
+    spy.assert_called_with(data)
+
+
 def test_db_add_actions_adapter_calls_with_correct_values(mocker: MockFixture):
     [sut, add_actions_repository] = make_sut()
     spy = mocker.spy(add_actions_repository, 'add')
