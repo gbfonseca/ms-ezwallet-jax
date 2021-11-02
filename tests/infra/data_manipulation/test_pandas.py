59d2aed5bf03bf236de3e6ca2c50a38764990d10
Gabriel Fonseca <gabriel.fonsecadev@gmail.com>
2021-11-02 20:30:46 -0300

    test: ensure DataManipulationAdapter calls to_dict with correct value

--- tests/infra/data_manipulation/test_pandas.py
+++ tests/infra/data_manipulation/test_pandas.py
@@ -16,3 +16,14 @@ def test_should_return_a_list_of_dicts_on_success(mocker: MockFixture):
     response = sut.to_dict(data)
 
     assert type(response) == list
+
+
+def test_should_calls_DataManipulationAdapter_with_correct_value(mocker: MockFixture):
+    sut = make_sut()
+    spy = mocker.spy(sut, 'to_dict')
+
+    data = html_string
+
+    sut.to_dict(data)
+
+    spy.assert_called_with(data)
