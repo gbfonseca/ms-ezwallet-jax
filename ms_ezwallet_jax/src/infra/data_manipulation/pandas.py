b3c38bb5ca5d4c6a44664ca52d34a6cc50d38c37
Gabriel Fonseca <gabriel.fonsecadev@gmail.com>
2021-11-25 21:50:44 -0300

    feat: save only 'Papel' of stock

--- ms_ezwallet_jax/src/infra/data_manipulation/pandas.py
+++ ms_ezwallet_jax/src/infra/data_manipulation/pandas.py
@@ -6,8 +6,4 @@ import pandas as pd
 class DataManipulationAdapter(DataManipulation):
     def to_dict(self, data: str):
         df = pd.read_html(data)[0]
-        df = pandas_helper.remove_percent(df)
-        df = pandas_helper.convert_fields_to_number(df)
-        df = pandas_helper.convert_price_to_correct_value(df)
-        df = pandas_helper.remap_field_values(df)
-        return df.to_dict('records')
+        return df['Papel'].to_frame().to_dict('records')
