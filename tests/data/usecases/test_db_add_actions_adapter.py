56ae7c7d337c9e0302edf8c2b8677f818de5dff2
Gabriel Fonseca <gabriel.fonsecadev@gmail.com>
2021-11-29 14:01:23 -0300

    refactor: db_add_actions_adapter tests

--- tests/data/usecases/test_db_add_actions_adapter.py
+++ tests/data/usecases/test_db_add_actions_adapter.py
@@ -9,27 +9,7 @@ def make_add_actions_repository() -> AddActionsRepository:
             return [
                 {
                     "_id": 'any_id',
-                    "Papel": "BBAS3",
-                    "Cotação": 2917,
-                    "P/L": 526,
-                    "P/VP": "062",
-                    "PSR": "0000",
-                    "Div.Yield": "6,88%",
-                    "P/Ativo": 0,
-                    "P/Cap.Giro": "000",
-                    "P/EBIT": "000",
-                    "P/Ativ Circ.Liq": 0,
-                    "EV/EBIT": "000",
-                    "EV/EBITDA": "000",
-                    "Mrg Ebit": "0,00%",
-                    "Mrg. Líq.": "0,00%",
-                    "Liq. Corr.": 0,
-                    "ROIC": "0,00%",
-                    "ROE": "11,73%",
-                    "Liq.2meses": "419.635.000,00",
-                    "Patrim. Líq": "135.444.000.000,00",
-                    "Dív.Brut/ Patrim.": "000",
-                    "Cresc. Rec.5a": "0,79%"
+                    "code": "BBAS3",
                 }
             ]
     return AddActionsRepositoryStub()
@@ -46,27 +26,7 @@ def test_db_add_actions_adapter_returns_an_actions_on_success(mocker: MockFixtur
 
     data = [
         {
-            "Papel": "BBAS3",
-            "Cotação": 2917,
-            "P/L": 526,
-            "P/VP": "062",
-            "PSR": "0000",
-            "Div.Yield": "6,88%",
-            "P/Ativo": 0,
-            "P/Cap.Giro": "000",
-            "P/EBIT": "000",
-            "P/Ativ Circ.Liq": 0,
-            "EV/EBIT": "000",
-            "EV/EBITDA": "000",
-            "Mrg Ebit": "0,00%",
-            "Mrg. Líq.": "0,00%",
-            "Liq. Corr.": 0,
-            "ROIC": "0,00%",
-            "ROE": "11,73%",
-            "Liq.2meses": "419.635.000,00",
-            "Patrim. Líq": "135.444.000.000,00",
-            "Dív.Brut/ Patrim.": "000",
-            "Cresc. Rec.5a": "0,79%"
+            "code": "BBAS3",
         }
     ]
 
@@ -75,27 +35,7 @@ def test_db_add_actions_adapter_returns_an_actions_on_success(mocker: MockFixtur
     assert response == [
         {
             "_id": 'any_id',
-            "Papel": "BBAS3",
-            "Cotação": 2917,
-            "P/L": 526,
-            "P/VP": "062",
-            "PSR": "0000",
-            "Div.Yield": "6,88%",
-            "P/Ativo": 0,
-            "P/Cap.Giro": "000",
-            "P/EBIT": "000",
-            "P/Ativ Circ.Liq": 0,
-            "EV/EBIT": "000",
-            "EV/EBITDA": "000",
-            "Mrg Ebit": "0,00%",
-            "Mrg. Líq.": "0,00%",
-            "Liq. Corr.": 0,
-            "ROIC": "0,00%",
-            "ROE": "11,73%",
-            "Liq.2meses": "419.635.000,00",
-            "Patrim. Líq": "135.444.000.000,00",
-            "Dív.Brut/ Patrim.": "000",
-            "Cresc. Rec.5a": "0,79%"
+            "code": "BBAS3",
         }
     ]
 
@@ -107,27 +47,7 @@ def test_db_add_actions_adapter_calls_add_with_correct_value(mocker: MockFixture
 
     data = [
         {
-            "Papel": "BBAS3",
-            "Cotação": 2917,
-            "P/L": 526,
-            "P/VP": "062",
-            "PSR": "0000",
-            "Div.Yield": "6,88%",
-            "P/Ativo": 0,
-            "P/Cap.Giro": "000",
-            "P/EBIT": "000",
-            "P/Ativ Circ.Liq": 0,
-            "EV/EBIT": "000",
-            "EV/EBITDA": "000",
-            "Mrg Ebit": "0,00%",
-            "Mrg. Líq.": "0,00%",
-            "Liq. Corr.": 0,
-            "ROIC": "0,00%",
-            "ROE": "11,73%",
-            "Liq.2meses": "419.635.000,00",
-            "Patrim. Líq": "135.444.000.000,00",
-            "Dív.Brut/ Patrim.": "000",
-            "Cresc. Rec.5a": "0,79%"
+            "code": "BBAS3",
         }
     ]
 
@@ -141,27 +61,7 @@ def test_db_add_actions_adapter_calls_with_correct_values(mocker: MockFixture):
     spy = mocker.spy(add_actions_repository, 'add')
     data = [
         {
-            "Papel": "BBAS3",
-            "Cotação": 2917,
-            "P/L": 526,
-            "P/VP": "062",
-            "PSR": "0000",
-            "Div.Yield": "6,88%",
-            "P/Ativo": 0,
-            "P/Cap.Giro": "000",
-            "P/EBIT": "000",
-            "P/Ativ Circ.Liq": 0,
-            "EV/EBIT": "000",
-            "EV/EBITDA": "000",
-            "Mrg Ebit": "0,00%",
-            "Mrg. Líq.": "0,00%",
-            "Liq. Corr.": 0,
-            "ROIC": "0,00%",
-            "ROE": "11,73%",
-            "Liq.2meses": "419.635.000,00",
-            "Patrim. Líq": "135.444.000.000,00",
-            "Dív.Brut/ Patrim.": "000",
-            "Cresc. Rec.5a": "0,79%"
+            "code": "BBAS3",
         }
     ]
 
@@ -175,27 +75,7 @@ def test_db_add_actions_adapter_returns_item_action_with_id(mocker: MockFixture)
 
     data = [
         {
-            "Papel": "BBAS3",
-            "Cotação": 2917,
-            "P/L": 526,
-            "P/VP": "062",
-            "PSR": "0000",
-            "Div.Yield": "6,88%",
-            "P/Ativo": 0,
-            "P/Cap.Giro": "000",
-            "P/EBIT": "000",
-            "P/Ativ Circ.Liq": 0,
-            "EV/EBIT": "000",
-            "EV/EBITDA": "000",
-            "Mrg Ebit": "0,00%",
-            "Mrg. Líq.": "0,00%",
-            "Liq. Corr.": 0,
-            "ROIC": "0,00%",
-            "ROE": "11,73%",
-            "Liq.2meses": "419.635.000,00",
-            "Patrim. Líq": "135.444.000.000,00",
-            "Dív.Brut/ Patrim.": "000",
-            "Cresc. Rec.5a": "0,79%"
+            "code": "BBAS3",
         }
     ]
 
