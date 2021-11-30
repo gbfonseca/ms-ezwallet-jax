c0948b465707e28bbddbcb274741d1f2bff3c067
Gabriel Fonseca <gabriel.fonsecadev@gmail.com>
2021-11-29 21:11:46 -0300

    test: ensure GetActionsDataByCodeController return 200 on success

--- tests/presentation/controllers/actions/test_get_action_data_by_code.py
+++ tests/presentation/controllers/actions/test_get_action_data_by_code.py
@@ -1,9 +1,21 @@
 from pytest_mock import MockFixture
 from ms_ezwallet_jax.src.presentation.controllers.actions.get_action_data_by_code import GetActionDataByCodeController
+from ms_ezwallet_jax.src.domain.usecases.http_client import HttpClient
+from tests.factories.action import action
+
+
+class HttpClientStub(HttpClient):
+    def get(self, url: str):
+        return action
+
+
+def make_http_client() -> HttpClient:
+    return HttpClientStub()
 
 
 def make_sut() -> GetActionDataByCodeController:
-    sut = GetActionDataByCodeController()
+    http_client_stub = make_http_client()
+    sut = GetActionDataByCodeController(http_client_stub)
     return sut
 
 
@@ -18,3 +30,16 @@ def test_should_return_400_if_code_not_provided(mocker: MockFixture):
     response = sut.handle(http_request)
     assert response['status_code'] == 400
     assert response['body']['message'] == 'Código obrigatório.'
+
+
+def test_should_return_200_on_success(mocker: MockFixture):
+    sut = make_sut()
+
+    http_request = {
+        'params': {
+            'code': 'bbas3.sa'
+        }
+    }
+    response = sut.handle(http_request)
+
+    assert response['status_code'] == 200
