dbb5d86e7d53ce56d0933c1d55e18cc1d5ea4637
Gabriel Fonseca <gabriel.fonsecadev@gmail.com>
2021-11-29 21:43:50 -0300

    test: ensure RequestsAdapter return a data on success

--- tests/infra/http_client/test_requests.py
+++ tests/infra/http_client/test_requests.py
@@ -1,7 +1,7 @@
 from pytest_mock import MockFixture
 from ms_ezwallet_jax.src.infra.http_client.requests import RequestsAdapter
 from ms_ezwallet_jax.src.domain.usecases.http_client import HttpClient
-
+from tests.factories.action import action
 FAKE_URL = 'https://any_url.com'
 
 
@@ -15,3 +15,12 @@ def test_should_return_an_exception_if_no_url_provided(mocker: MockFixture):
     response = sut.get(URL)
 
     assert type(response) == Exception
+
+
+def test_should_return_a_data_on_success(mocker: MockFixture):
+    sut = make_sut()
+    spy = mocker.spy(sut, 'get')
+    spy.side_effect = action
+    response = sut.get(FAKE_URL)
+
+    assert response is not None
