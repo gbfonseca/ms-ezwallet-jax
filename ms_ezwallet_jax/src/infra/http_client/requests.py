d99c8dac3063fca930e9df40e1d9d299d496018c
Gabriel Fonseca <gabriel.fonsecadev@gmail.com>
2021-11-29 21:42:55 -0300

    feat: ensure RequestsAdapter return a data on success

--- ms_ezwallet_jax/src/infra/http_client/requests.py
+++ ms_ezwallet_jax/src/infra/http_client/requests.py
@@ -1,9 +1,13 @@
 from ms_ezwallet_jax.src.domain.usecases.http_client import HttpClient
+import requests
 
 
 class RequestsAdapter(HttpClient):
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
 
     def get(self, url: str) -> any:
         if(url is None):
             return Exception('Url obrigatória.')
-        return
+        data = requests.get(url=url, headers=self.headers)
+        return data.json()
