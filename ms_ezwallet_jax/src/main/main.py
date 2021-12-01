349322d1852cf8940db7c6196d739271e19170b0
Gabriel Fonseca <gabriel.fonsecadev@gmail.com>
2021-11-30 22:57:00 -0300

    chore: add poetry start script

--- ms_ezwallet_jax/src/main/main.py
+++ ms_ezwallet_jax/src/main/main.py
@@ -1,6 +1,16 @@
 from fastapi import FastAPI
 from ms_ezwallet_jax.src.main.routes.actions import actions_router
+import uvicorn
 
 app = FastAPI()
 
 app.include_router(actions_router)
+
+
+def start():
+    uvicorn.run("ms_ezwallet_jax.src.main.main:app",
+                host="0.0.0.0", port=8000, reload=True)
+
+
+if __name__ == '__main__':
+    start()
