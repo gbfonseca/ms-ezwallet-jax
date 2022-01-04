acd0990045ff9ba5fe69026fe3264d2bb67d477b
Gabriel Fonseca <gabriel.fonsecadev@gmail.com>
2022-01-03 22:13:16 -0300

    chore: remove webdriver

--- ms_ezwallet_jax/src/infra/scraper/selenium.py
+++ ms_ezwallet_jax/src/infra/scraper/selenium.py
@@ -1,34 +1,16 @@
 import pandas as pd
 from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.common.by import By
 
 from ms_ezwallet_jax.src.domain.usecases.scraper import Scraper
 from ms_ezwallet_jax.src.domain.usecases.html_parser import HtmlParser
 from ms_ezwallet_jax.src.domain.usecases.data_manipulation import DataManipulation
-from ms_ezwallet_jax.src.main.config.configuration import configuration
-from webdriver_manager.chrome import ChromeDriverManager
 
-def load_driver():
-    options = webdriver.ChromeOptions()
-    options.headless = True
-    options.add_argument('--disable-gpu')
-    options.add_argument('--no-sandbox')
-    if configuration['PYTHON_ENV'] != 'development':
-        options.binary_location = configuration['GOOGLE_CHROME_PATH']
-        
-        driver = webdriver.Chrome(
-            executable_path=configuration['CHROMEDRIVER_PATH'],
-            options=options,
-            )
-        return driver
-    else:
-        driver = webdriver.Chrome(
-            executable_path=ChromeDriverManager().install(),
-            options=options,
-            )
-        return driver
+options = Options()
+options.headless = True
+driver = webdriver.Firefox(options=options)
 
-driver = load_driver()
 
 class ScraperAdapter(Scraper):
 
