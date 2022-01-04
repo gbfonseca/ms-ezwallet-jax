62bb92aceb0f699d1500bf310d72ba2fc8ba50ea
Gabriel Fonseca <gabriel.fonsecadev@gmail.com>
2022-01-04 11:43:49 -0300

    feat: insert sorted on database

--- ms_ezwallet_jax/src/infra/data_manipulation/pandas.py
+++ ms_ezwallet_jax/src/infra/data_manipulation/pandas.py
@@ -5,8 +5,10 @@ import pandas as pd
 
 class DataManipulationAdapter(DataManipulation):
     def to_dict(self, data: str):
-        df = pd.read_csv('./acoes.csv', encoding='ISO-8859-1',  skiprows=2, delimiter=';')
+        df = pd.read_csv('./acoes.csv', encoding='ISO-8859-1',
+                         skiprows=2, delimiter=';')
         new_df = pd.DataFrame(df['Empresa'].values, columns=['code'])
+        new_df = new_df.sort_values(by='code', ascending=True)
         codes_with_flag_dict = pandas_helper.insert_country_flag(
             new_df, 'SA').to_dict('records')
         return codes_with_flag_dict
