0c7ce82ab37cda44ea98a7538db38214d5974ed4
Gabriel Fonseca <gabriel.fonsecadev@gmail.com>
2022-01-05 12:20:11 -0300

    feat: remove responsability of generate uuid of the mongo

--- ms_ezwallet_jax/src/infra/db/mongodb/repositories/actions/actions.py
+++ ms_ezwallet_jax/src/infra/db/mongodb/repositories/actions/actions.py
@@ -1,6 +1,7 @@
 from ms_ezwallet_jax.src.data.protocols.add_actions_repository import AddActionsRepository
 from ms_ezwallet_jax.src.infra.db.mongodb.helpers.mongo_helper import mongo_helper
 from pymongo import UpdateOne
+import uuid
 
 
 class ActionsRepository(AddActionsRepository):
@@ -10,13 +11,14 @@ class ActionsRepository(AddActionsRepository):
         saved_actions = []
         requests = []
         for action in data:
+            action['_id'] = str(uuid.uuid4())
             requests.append(UpdateOne({'code': action['code']}, {
-                '$set':  action}, upsert=True))
+                '$set':  {'code': action['code']}}, upsert=True))
         actions_collection.bulk_write(requests)
         actions = actions_collection.find()
         for document in actions:
             saved_actions.append({
-                'id': str(document['_id']),
+                'id': document['_id'],
                 'code': document['code']
             })
         return saved_actions
